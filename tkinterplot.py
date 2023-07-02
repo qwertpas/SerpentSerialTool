b592cf159bedd6d247cdcaca727a49ef6c8ef0d1
Christopher <christopher.y.x@gmail.com>
2023-07-01 19:26:27 -0500

    good axis marks

--- tkinterplot.py
+++ tkinterplot.py
@@ -13,18 +13,18 @@ class Plot(tk.Frame):
         self.canvas = tk.Canvas(self, background="gray12")
         self.canvas.bind("<Configure>", self.on_resize)
 
-        self.canvas.create_line((0, 0, 0, 0), tag='X', fill='darkblue', width=1)
-        self.canvas.create_line((0, 0, 0, 0), tag='Y', fill='darkred', width=1)
-        self.canvas.create_line((0, 0, 0, 0), tag='Z', fill='darkgreen', width=1)
+        self.yaxis_frame = tk.Frame(parent)
+        self.yaxis_frame.pack()
 
-        legend_frame = tk.LabelFrame(parent,text='Legend',padx=5, pady=5)
-        legend_frame.pack()
+        self.side_frame = tk.LabelFrame(parent,text='Scale',padx=5, pady=5)
+        self.side_frame.pack(side=tk.RIGHT, fill='both')
 
-        self.legend_label = tk.Label(legend_frame, text="sdsdsds")
+        self.legend_label = tk.Label(self.side_frame, text="set the sacle")
         self.legend_label.pack()
 
         self.canvas.pack(expand=True, fill=tk.BOTH)
         self.pack(expand=True, fill=tk.BOTH)
+
 
         self.historylen = 100
         self.labels = []
@@ -32,20 +32,44 @@ class Plot(tk.Frame):
         # self.scale = np.array([])
         self.hues = []
         self.w = parent.winfo_width()
-        self.h_2 = parent.winfo_height() / 2.0
+        self.h_2 = parent.winfo_height() / 2.
 
         self.message = "" #buffer that some external loop updates, then the plotter displays it periodically
 
-        self.canvas.create_line(0, 0, 0, 0, tag="X=0")
+        self.textoffset = np.array([-10, 0]) #offsets for the labels
+
+        self.temp_tags = [] #strings of y axis mark tags that get redrawn on resize
 
         self.plotloop()
         
+    def disp(self, val):
+        pad = 20
+        return (self.h_2 - pad) * (1 - val) + pad
+    def invdisp(self, disp):
+        pad = 20
+        return 1 - (disp-pad)/(self.h_2-pad)
 
     def on_resize(self, event=None):
         self.w = self.winfo_width()
-        self.h_2 = self.winfo_height()/2.
-        self.canvas.coords("X=0", 0, self.h_2, self.w, self.h_2)
-        self.data = np.zeros_like(self.data)
+        self.h_2 = self.winfo_height() / 2.
+
+        for temp_tag in self.temp_tags:
+            self.canvas.delete(temp_tag)  # Delete line item from the canvas
+
+        for y in np.arange(-1, 1.01, 0.2):
+            y = round(y, 2)
+            marktag = f"_M{y}"
+            gridtag = f"_G{y}"
+            self.temp_tags.append(marktag)
+            self.temp_tags.append(gridtag)
+            h = int(self.disp(y))
+            if(y==0):
+                self.canvas.create_line(0, self.disp(y), self.w, self.disp(y), tag=gridtag, fill="#AAAAAA")
+            else:
+                self.canvas.create_line(0, self.disp(y), self.w, self.disp(y), tag=gridtag, fill="#454545")
+            self.canvas.create_text(10, h, anchor='w', text=f"{y}", tag=marktag)
+
+        # self.data = np.zeros_like(self.data)
         self.draw()
 
     def str_to_data(message:str):
@@ -58,8 +82,11 @@ class Plot(tk.Frame):
                 if len(parts) != 2: #ignore lines that don't have exactly one colon
                     continue
                 label, value = parts
+                label = label.strip()
+                if label in new_labels:
+                    continue        #is a duplicate entry in the same message
                 value = float(value.strip().split(" ")[0]) #only use the first "word" and ignore spaces
-                new_labels.append(label.strip())
+                new_labels.append(label)
                 new_data.append(value)
             except Exception as e:
                 print(e)
@@ -92,6 +119,7 @@ class Plot(tk.Frame):
                 if self.labels[i] not in new_labels:
                     self.data = np.delete(self.data, i, axis=0)
                     self.canvas.delete(self.labels[i])
+                    self.canvas.delete(f"{self.labels[i]}L")
                     to_delete.append(i)
             for i in to_delete:
                 print(f"removing {self.labels[i]}")
@@ -106,7 +134,13 @@ class Plot(tk.Frame):
                         self.data = np.append(self.data, [np.zeros(self.historylen)], axis=0)
                     else:
                         self.data = np.array([np.zeros(self.historylen)])
-                    self.canvas.create_line(0,0,0,0, tag=new_label)
+                    self.canvas.create_line(0,0,0,0, tag=f"{new_label}L")
+                    self.canvas.create_text(0, 0, anchor="e", tag=f"{new_label}T", text=new_label)
+
+                    
+                    new_scaleinput = tk.Entry(self.side_frame)
+                    new_scaleinput.pack()
+
                     added_new = True
                     print(f"added new series: {new_label}")
 
@@ -115,9 +149,10 @@ class Plot(tk.Frame):
                 m = len(self.labels)
                 hues = np.linspace(0, 1, m + 1) #last item is 1, which is the same hue as 0 so it is unused
                 for i in range(m):
-                    self.canvas.itemconfig(self.labels[i], fill=Plot.hue_to_hex(hues[i]))
+                    self.canvas.itemconfig(f"{self.labels[i]}L", fill=Plot.hue_to_hex(hues[i]))
+                    self.canvas.itemconfig(f"{self.labels[i]}T", fill=Plot.hue_to_hex(hues[i]))
 
-            # print(f"concating {[self.data, np.array([new_data])[:,np.newaxis]]}")
+            # print(f"concating {[self.data, np.array([new_data])[:,np.newaxis]]}")   
             self.data = np.concatenate([self.data[:,1:], np.array(new_data)[:,np.newaxis]], axis=1)   #combine shifted old data and new data
             self.after_idle(self.draw)
 
@@ -132,7 +167,8 @@ class Plot(tk.Frame):
             return
 
         # data_scaled = self.data @ self.scale
-        data_scaled = self.h_2 * (1 - self.data) #scale data y-values for the display
+        # data_scaled = self.h_2 * (1 - self.data) #scale data y-values for the display
+        data_scaled = self.disp(self.data)
 
         #canvas.coords() takes in a flattened input: x1,y1,x2,y2,... 
         #next 3 lines adds a column vectors to every other index of the data 2D array which are the x values.
@@ -142,12 +178,13 @@ class Plot(tk.Frame):
         # print("xvals_arr: \n", xvals_arr)
         # print("data_scaled: \n", data_scaled)
         
-        points = np.dstack([xvals_arr, data_scaled]).reshape(m, 2*n) 
+        points = np.dstack([xvals_arr, data_scaled]).reshape(m, 2*n)
 
         for i in range(len(self.labels)):
-            self.canvas.coords(self.labels[i], points[i].tolist())
+            self.canvas.coords(f"{self.labels[i]}L", points[i].tolist())
+            self.canvas.coords(f"{self.labels[i]}T", (points[i][-2:] + self.textoffset).tolist())
 
-t = 0
+count = 0
 def main():
     np.set_printoptions(precision=2, suppress=True)
 
@@ -155,14 +192,15 @@ def main():
     plot = Plot(root)
 
     def senddata():
-        global t
-        t += 0.01
+        global count
+        count += 1
 
-        if(t < 5):
-            plot.set(message=f"rand1: {np.sin(t)} \n rand2: {np.cos(t)}")
-        else:
-            plot.set(message=f"rand1: {np.sin(t)} \n rand3: {np.cos(t)} \n rand4: {np.tan(t)}")
-
+        message = ""
+        for i in range((int)(count / 200)+1):
+            if(i > 4):
+                break
+            message = message + f"{i}: {np.sin(count/50+i)} \n"
+        plot.set(message)
     datastream = PeriodicSleeper(senddata, 0.01)
 
     root.mainloop()
